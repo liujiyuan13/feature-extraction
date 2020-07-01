1e27a04ac4589a5c6b28c05431896e436b841dce
Jiyuan <jiyuan@kenan.kenan.com>
2020-07-02 00:51:22 +0800

    cache

--- mydemo.m
+++ mydemo.m
@@ -9,6 +9,11 @@ openPool(c.cores);
 % dpath = 'D:\Work\datasets\mData\OrigData\';
 dpath = '/home/ftp2/jiyuan/datasets/';
 dnames = {'MNIST', 'FashionMNIST', 'CIFAR10', 'CIFAR100', 'STL10'};
+Fshapes = {{[77,50], [1,512], [25,124], [25,279], [1,1239], [26,128], [784,30]}, ...
+          {[77,50], [1,512], [25,124], [25,279], [1,1239], [26,128], [784,30]}, ...
+          {[110,50], [1,512], [36,124], [36,279], [1,1239], [38,128], [1024,30]}, ...
+          {[110,50], [1,512], [36,124], [36,279], [1,1239], [38,128], [1024,30]}, ...
+          {[1454,50], [1,512], [256,124], [256,279], [1,1239], [590,128], [9216,30]}};
 len_dn = length(dnames);
 
 view_meaning = {'color', 'gist', 'hog2x2', 'hog3x3', 'lbp', 'sift', 'ssim'}';
@@ -26,30 +31,27 @@ for dn = 1:len_dn
     
     Xtmp = cell(numfeat, 1);
     infos = cell(numfeat, 1);
-    Fshape = cell(numfeat, 1);
     for f = 1:numfeat
         feature = view_meaning{f};
+        fprintf('\n# feature: %s', feature);
         tmpF = zeros(0);
         tmpinfo = cell(0);
         parfor i = 1:numsmp
             img = squeeze(X(i,:,:,:));
             [tmp, tmpinfo{i}.x, tmpinfo{i}.y, tmpinfo{i}.wid, tmpinfo{i}.hgt] = extract_feature(feature, img);
-%             Fshape{1} = [size(tmp,1), size(tmp,2)]; % N_d x d
+%             Fshape{f} = [size(tmp,1), size(tmp,2)]; % N_d x d
             tmpF(:,i) = reshape(tmp', size(tmp,1)*size(tmp,2), 1);
         end
         Xtmp{f} = double(tmpF);
         infos{f} = tmpinfo;
     end
-    class_meaning = class_meaning';
     data_name = dname;
+    Fshape = Fshapes{dn}';
+    X = Xtmp;
 
     fprintf('\n\n# save fea mat of %s', dname);
     
-%     save([dpath, dname, '\', dname, '_img.mat'], 'data_name', 'X', 'Y', 'class_meaning', 'view_meaning', '-v7.3')
-    save([dpath, 'mData/Fmatrix/', dname, '_img.mat'], 'data_name', 'X', 'Y', 'infos', 'class_meaning', 'view_meaning', '-v7.3');
-    
-    % manually add Fshape
-%     save([dpath, 'mData/Fmatrix/', dname, '_img.mat'], 'data_name', 'X', 'Y', 'infos', 'Fshape', 'class_meaning', 'view_meaning', '-v7.3');
+    save([dpath, 'mData/Fmatrix/', dname, '_fea.mat'], 'data_name', 'X', 'Y', 'infos', 'Fshape', 'class_meaning', 'view_meaning', '-v7.3');
             
     
 end
